8b8612dcad44e0eda5a140195efcf9166f568930
Mao-Syuan <37368872+ChiuMike@users.noreply.github.com>
2021-09-21 16:49:10 +0800

    first

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/e-commerce-website/',
+    assetsPublicPath: '/e-commerce-web/',
 
     /**
      * Source Maps
